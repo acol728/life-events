0ead05b30fd56d7c461d0cf571c87a9e8fecae03
Scott Butler <scottbutler@northwesternmutual.com>
2018-06-05 19:08:24 -0500

    changed colors

--- src/js/navigation/index.js
+++ src/js/navigation/index.js
@@ -12,7 +12,8 @@ const showAndHideLowerNav = (pageIndex, numberOfPages) => {
 }
 
 const highlightSideNav = (page, show) => {
-	show ? $(`#nav-${page.id}`).parent().addClass('active') : $(`#nav-${page.id}`).parent().removeClass('active')
+	// show ? $(`#nav-${page.id}`).parent().addClass('active') : $(`#nav-${page.id}`).parent().removeClass('active')
+	show ? $(`#nav-${page.id}`).css({ 'background-color': '#16b161', color: 'white' }) : $(`#nav-${page.id}`).css({ 'background-color': 'transparent', color: '#9c9c9c' })
 }
 
 const updatePages = (pages, pageIndex) => (pages || []).map((page, i) => {
