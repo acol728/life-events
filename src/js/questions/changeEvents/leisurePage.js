fc8f1cb72c483aba1a799e040aea85d0b71df6b2
myerabig <myerabig@aol.com>
2018-06-22 09:41:32 -0500

    fixed radios and added to sidebar

--- src/js/questions/changeEvents/leisurePage.js
+++ src/js/questions/changeEvents/leisurePage.js
@@ -1,21 +1,20 @@
+/*
 import CONSTANTS from '../constants';
+import { addOrUpdateInfo } from './util';
+import pages from '../pages';
 
 const { PAGE_IDS, QUESTION_IDS } = CONSTANTS.IDs;
 const { LEISURE_PAGE } = PAGE_IDS;
 
 export default {
-  [QUESTION_IDS[LEISURE_PAGE].NUMBER_OF_VACATIONS_DROPDOWN]: (e) => {
-    // const numOfVacations = e.target.value;
-    // console.log(e);
-  },
-  [QUESTION_IDS[LEISURE_PAGE].LENGTH_OF_VACATIONS_DROPDOWN]: (e) => {
-    // const lengthOfVacations = e.target.value;
-    // console.log(e);
-  },
-  [QUESTION_IDS[LEISURE_PAGE].SUBSCRIPTION_DROPDOWN]: (e) => {
-
-  },
-  [QUESTION_IDS[LEISURE_PAGE].COFFEE_RADIO]: (e) => {
-
+  [QUESTION_IDS[LEISURE_PAGE].NUMBER_OF_CARS_TEXT]: (e) => {
+    const infoItems = [
+      {
+        key: pages[3].questions[2].info,
+        val: parsedValue
+      }
+    ];
+    addOrUpdateInfo(infoItems);
   }
 };
+*/
---
+++ src/js/questions/changeEvents/lifestylePage.js
@@ -0,0 +1,51 @@
+import CONSTANTS from '../constants';
+import { showError, removeError, addOrUpdateInfo } from './util';
+import pages from '../pages';
+
+const { PAGE_IDS, QUESTION_IDS } = CONSTANTS.IDs;
+const { LIFESTYLE_PAGE } = PAGE_IDS;
+
+export default {
+  [QUESTION_IDS[LIFESTYLE_PAGE].NUMBER_OF_CARS_TEXT]: (e) => {
+    const parsedValue = parseInt(e.target.value, 10);
+    let isValid = !Number.isNaN(parsedValue);
+
+    isValid = !!(isValid && (parsedValue >= 0 && parsedValue <= 100));
+
+    if (isValid) {
+      state.ui.values[QUESTION_IDS[LIFESTYLE_PAGE].NUMBER_OF_CARS_TEXT] = parsedValue;
+      state.ui.values.info[pages[1].questions[0].info] = parsedValue;
+      const infoItems = [
+        {
+          key: pages[3].questions[2].info,
+          val: parsedValue
+        }
+      ];
+      addOrUpdateInfo(infoItems);
+      removeError(QUESTION_IDS[LIFESTYLE_PAGE].NUMBER_OF_CARS_TEXT);
+    } else {
+      showError(QUESTION_IDS[LIFESTYLE_PAGE].NUMBER_OF_CARS_TEXT, 'Invalid Amount');
+    }
+  },
+  [QUESTION_IDS[LIFESTYLE_PAGE].NUMBER_OF_DEPENDENCIES_TEXT]: (e) => {
+    const parsedValue = parseInt(e.target.value, 10);
+    let isValid = !Number.isNaN(parsedValue);
+
+    isValid = !!(isValid && (parsedValue >= 0 && parsedValue <= 100));
+
+    if (isValid) {
+      state.ui.values[QUESTION_IDS[LIFESTYLE_PAGE].NUMBER_OF_DEPENDENCIES_TEXT] = parsedValue;
+      state.ui.values.info[pages[1].questions[0].info] = parsedValue;
+      const infoItems = [
+        {
+          key: pages[3].questions[3].info,
+          val: parsedValue
+        }
+      ];
+      addOrUpdateInfo(infoItems);
+      removeError(QUESTION_IDS[LIFESTYLE_PAGE].NUMBER_OF_DEPENDENCIES_TEXT);
+    } else {
+      showError(QUESTION_IDS[LIFESTYLE_PAGE].NUMBER_OF_DEPENDENCIES_TEXT, 'Invalid Amount');
+    }
+  }
+};
